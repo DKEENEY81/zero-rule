22b6957b0f41999ead049ab1a9775f1a74d10b80
Donald Keeney <keeney@Donalds-MacBook-Air.local>
2019-06-06 11:22:17 -0400

    main function and added shebangs

--- 0rule.py
+++ 0rule.py
@@ -1,4 +1,4 @@
-
+#!/usr/local/bin/python3
 from random import seed
 from random import randrange
  
---
+++ mainfxn.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3
+
+print("Hello")
+
+print("__name__ value: ", __name__)
+
+
+
+def main():
+	print("main fxn")
+
+
+
+if __name__ =='__main__':
+	main()
